6745abb592d506f66a70d88fcee51d0ae2840a22
Tobias Frantsen <tof@amsiq.com>
2018-10-10 13:02:58 +0200

    Install Pod

--- Pods/Target Support Files/Pods-MultimediaChat/Pods-MultimediaChat-umbrella.h
+++ Pods/Target Support Files/Pods-MultimediaChat/Pods-MultimediaChat-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_CollectionViewVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CollectionViewVersionString[];
+FOUNDATION_EXPORT double Pods_MultimediaChatVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MultimediaChatVersionString[];
 
