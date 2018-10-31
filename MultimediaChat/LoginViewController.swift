73efc2ad98efb44501a1d1fca83d2d17af83d2e5
Tobias Frantsen <tof@amsiq.com>
2018-10-31 15:05:40 +0100

    LoginController: Now remebmer username

--- MultimediaChat/LoginViewController.swift
+++ MultimediaChat/LoginViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Tobias Frantsen on 23/10/2018.
 //  Copyright © 2018 Tobias Frantsen. All rights reserved.
 //
-
+import BRYXBanner
 import UIKit
 
 class LoginViewController: UIViewController {
@@ -14,13 +14,27 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         SocketIOManager.shared.connectSocket()
+        
+        if let name = UserDefaults.standard.string(forKey: "userName") {
+            let banner = Banner(title: "Last Login with \(name)", subtitle: "Change if you want", backgroundColor: .blue)
+            banner.dismissesOnTap = true
+            banner.show(duration: 3.0)
+            self.userNameTextField.text = name
+            
+        }
         // Do any additional setup after loading the view.
     }
     
 
     @IBAction func tapLogin(_ sender: Any) {
         
-        SocketIOManager.shared.connectToServerWithUserName(nickname: self.userNameTextField.text!)
+        guard let userName = self.userNameTextField.text, userName != "" else {
+            return
+        }
+        
+        UserDefaults.standard.set(userName, forKey: "userName")
+        
+        SocketIOManager.shared.connectToServerWithUserName(nickname: userName)
         
         
         if let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "mainViewController") as? ViewController {
